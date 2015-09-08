418cf54626065247f916d4688ef9e7b827e715ea
CHELSEAROYER <chelsearoyer1990@gmail.com>
2015-09-08 17:57:42 -0500

    File config, header js, test alert
    -Made new directory, js, & html files
    -created a header on js file, & alert to let me know if this is working
    properly.

---
+++ Dano_Chelsea_Assignment_Expressions/Expressions.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ChelseaRoyer on 9/8/15.
+ */
+/* Chelsea Dano
+SDI Section -week two expressions assignment
+Dano_Chelsea_Assignment_Expressions!
+8 September 2015
+ */
+alert("Time for a beer because your html and js file are now associated!");
+
