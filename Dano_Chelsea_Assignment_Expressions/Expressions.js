6d66c9563b8d2b85c632ec544fe1822b363af97a
CHELSEAROYER <chelsearoyer1990@gmail.com>
2015-09-08 22:56:29 -0500

    name inquiry
    with var epithet, & an alert message.

--- Dano_Chelsea_Assignment_Expressions/Expressions.js
+++ Dano_Chelsea_Assignment_Expressions/Expressions.js
@@ -6,5 +6,13 @@ SDI Section -week two expressions assignment
 Dano_Chelsea_Assignment_Expressions!
 8 September 2015
  */
-alert("Time for a beer because your html and js file are now associated!");
+/* If you had to get takeout for lunch (on work days) every single day of the year,
+how much would it cost you?
+ */
+
+var epithet= prompt ("What's your name?");//ask the user what their name is
+alert (" Howdy "+ epithet +" ! Let's assume that you eat takeout for lunch every day you work.");
+//send an alert to give the user an idea about what the info required will pertain to.
+
+
 
